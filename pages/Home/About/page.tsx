b6a89bcf2bc366237078760725939f702b11024f
TomasNieto7 <tnieto07@alumnos.uaq.mx>
2024-05-25 18:41:29 -0600

    Added: AboutPage

---
+++ pages/Home/About/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full bg-slate-500'>
+        <div className='w-full h-96'></div>
+        <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
