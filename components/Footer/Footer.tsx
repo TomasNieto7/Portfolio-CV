676749ac78242c70a29919a2fa426aa4245d1f75
TomasNieto7 <tnieto07@alumnos.uaq.mx>
2024-05-25 22:36:39 -0600

    Fixed: About text

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='bg-[#121212] h-32 flex items-center justify-center py-24 lg:py-0'>
             <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between">
-                <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20">Webdev Warriors 2023 | All Rights Reserved</div>
+                <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20">STomasio 2024 | All Rights Reserved</div>
                 <div className="flex items-center space-x-10">
                     <p className="text-[16px] text-white opacity-20 cursor-pointer">Terms & Condition</p>
                     <p className="text-[18px] text-white opacity-20 cursor-pointer">Privacy Policy</p>
--- pages/Home/About/page.tsx
+++ pages/Home/About/page.tsx
@@ -15,7 +15,9 @@ const About = () => {
                             </h2>
                             <TextAnimation />
                         </div>
-                        <p className='text-[#F4F4F4]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe sint inventore consequuntur repudiandae consectetur natus ratione harum blanditiis laboriosam, labore dignissimos sit soluta voluptatem quia corporis facilis ipsum ex maxime.</p>
+                        <p className='text-[#F4F4F4] text-justify'>
+                            Soy un apasionado estudiante de informática con experiencia en el desarrollo web. Me especializo tanto en el Frontend como en el Backend. Disfruto creando interfaces web interactivas y me encanta la maquetación. Siempre estoy buscando aprender y mejorar mis habilidades.
+                        </p>
                         <div className='px-[2rem] w-56 rounded-xl hover:bg-white transition-all duration-200 py-[1rem] text-[18px]
                             font-bold uppercase bg-[#55e6a5] text-black flex items-center justify-center space-x-2'>
                             <button className='flex items-center justify-around w-full'>
